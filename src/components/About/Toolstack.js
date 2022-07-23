5f76eed63012cf4229a4c4aaf3ec25de9b27c396
Pritish-Sinha <bpb1789@gmail.com>
2022-07-23 18:14:04 +0530

    add components

---
+++ src/components/About/Toolstack.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  SiLinux,
+  SiVisualstudiocode,
+  SiHeroku,
+  SiGooglecolab
+} from "react-icons/si";
+
+import {
+  GiJupiter,
+  
+} from "react-icons/gi";
+
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiLinux />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <GiJupiter />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiGooglecolab />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiHeroku />
+      </Col>
+    </Row>
+  );
+}
+
+export default Toolstack;
